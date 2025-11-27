e131753194147cc56b9ea887296033176e58eaf8
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2025-11-27 13:09:29 -0400

    feat: Interface in Domain: IEventRepository

---
+++ Events/EventsMS.Domain/Interfaces/IEventRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventsMS.Domain.Entities;
+
+namespace EventsMS.Domain.Interfaces
+{
+    public interface IEventRepository
+    {
+        Task AddAsync(Event eventEntity, CancellationToken cancellationToken);
+        Task<Event?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<IEnumerable<Event>> GetAllAsync(CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
