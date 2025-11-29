341c5f921cbeb654a90821dc0ad113c812b6f099
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2025-11-28 23:31:11 -0400

    feat: Infrastructure add migrations

---
+++ Seating/SeatingMS.Infrastructure/Migrations/20251124225351_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeatingMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventSeats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SectionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Row = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CurrentUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LockExpirationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventSeats", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventSeats_EventId_SectionId",
+                table: "EventSeats",
+                columns: new[] { "EventId", "SectionId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventSeats");
+        }
+    }
+}
