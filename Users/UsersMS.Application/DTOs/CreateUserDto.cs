a2a5f40c6efdac2c0875be605ff6c2918830fdc5
AlphaCSR <cesarleon908@gmail.com>
2026-01-13 20:39:03 -0400

    feat(users): actualizar DTOs con nuevos atributos de perfil y preferencias - Expandir UserDto y UpdateUserDto con campos de perfil (teléfono, dirección, foto, ...). - Refactorizar CreateUserDto a record. - Añadir exclusión de cobertura de código para DTOs.

--- Users/UsersMS.Application/DTOs/CreateUserDto.cs
+++ Users/UsersMS.Application/DTOs/CreateUserDto.cs
@@ -4,5 +4,5 @@ using UsersMS.Shared.Enums;
 namespace UsersMS.Application.DTOs
 {
     [ExcludeFromCodeCoverage]
-    public record CreateUserDto(string FullName, string Email, string Password, UserRole Role);
+    public record CreateUserDto(string FullName, string Email, string Password, UserRole Role, string? KeycloakId = null);
 }
