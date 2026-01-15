674925325bcb55b31736f066d7c8b6910bdad0d7
AlphaCSR <cesarleon908@gmail.com>
2026-01-14 23:06:25 -0400

    feat(payments): implementar migraciones iniciales para base de datos de pagos - Crear esquema base para el microservicio de pagos. - Añadir soporte para almacenamiento de correos electrónicos en transacciones. - Configurar snapshot del modelo de datos para EF Core.

---
+++ Payments/PaymentsMS.Infrastructure/Migrations/20251226200940_AddEmailToPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentsMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Payments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Payments");
+        }
+    }
+}
